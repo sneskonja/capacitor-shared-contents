c0f98425599494aa01d0c7513a6e3e480504e475
Snezana Nikolic <snezana.nikolic@boutsourcing.com>
2024-06-03 13:15:37 +0200

    iOS disabled for short

--- ios/Plugin/SharedContents.swift
+++ ios/Plugin/SharedContents.swift
@@ -1,43 +1,43 @@
 import Foundation
 
-extension FileManager {
-  static func sharedContainerURL() -> URL {
-    return FileManager.default.containerURL(
-    // TODO: add as global config
-      forSecurityApplicationGroupIdentifier: ""
-    )!
-  }
-}
+// extension FileManager {
+//   static func sharedContainerURL() -> URL {
+//     return FileManager.default.containerURL(
+//     // TODO: add as global config
+//       forSecurityApplicationGroupIdentifier: ""
+//     )!
+//   }
+// }
 
-@objc public class SharedContents: NSObject {
-    @objc public func writeContents(_ value: String) {
-           let archiveURL = FileManager.sharedContainerURL().appendingPathComponent("contents.json")
-           print(">>> \(archiveURL)")
-           do {
-               try value.write(to: archiveURL, atomically: true, encoding: .utf8)
-           } catch {
-               print("Error: Can't write contents")
-            return
-        }
-    }
-
-    @objc public func removeContents(_ value: String) {
-        let archiveURL = FileManager.sharedContainerURL().appendingPathComponent("contents.json")
-         print(">>> \(archiveURL)")
-         do {
-           // TODO: implement iOS method
-         } catch {
-             print("Error: Can't write contents")
-             return
-         }
-    }
-
-    @objc public func updateWidgets(_ value: [String]) {
-             do {
-               // TODO: implement iOS method
-             } catch {
-                 print("Error:")
-                 return
-             }
-        }
-}
+// @objc public class SharedContents: NSObject {
+//     @objc public func writeContents(_ value: String) {
+//            let archiveURL = FileManager.sharedContainerURL().appendingPathComponent("contents.json")
+//            print(">>> \(archiveURL)")
+//            do {
+//                try value.write(to: archiveURL, atomically: true, encoding: .utf8)
+//            } catch {
+//                print("Error: Can't write contents")
+//             return
+//         }
+//     }
+//
+//     @objc public func removeContents(_ value: String) {
+//         let archiveURL = FileManager.sharedContainerURL().appendingPathComponent("contents.json")
+//          print(">>> \(archiveURL)")
+//          do {
+//            // TODO: implement iOS method
+//          } catch {
+//              print("Error: Can't write contents")
+//              return
+//          }
+//     }
+//
+//     @objc public func updateWidgets(_ value: [String]) {
+//              do {
+//                // TODO: implement iOS method
+//              } catch {
+//                  print("Error:")
+//                  return
+//              }
+//         }
+// }
--- ios/Plugin/SharedContentsPlugin.swift
+++ ios/Plugin/SharedContentsPlugin.swift
@@ -8,30 +8,33 @@ import Capacitor
 @objc(SharedContentsPlugin)
 public class SharedContentsPlugin: CAPPlugin {
 
-    private let implementation = SharedContents()
+    //private let implementation = SharedContents()
 
     @objc func writeContents(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.writeContents(value)
-        ])
+//         let value = call.getString("value") ?? ""
+//         call.resolve([
+//             "value": implementation.writeContents(value)
+//         ])
+        call.unimplemented("Not implemented on iOS.")
     }
 
     @objc func removeContents(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.removeContents(value)
-        ])
+//         let value = call.getString("value") ?? ""
+//         call.resolve([
+//             "value": implementation.removeContents(value)
+//         ])
+        call.unimplemented("Not implemented on iOS.")
     }
 
     @objc func updateWidgets(_ call: CAPPluginCall) {
-        guard let valueArray = call.getArray("value", String.self) else {
-                call.reject("Must provide an array of strings")
-                return
-            }
-
-            call.resolve([
-                "value": implementation.updateWidgets(valueArray)
-            ])
+//         guard let valueArray = call.getArray("value", String.self) else {
+//                 call.reject("Must provide an array of strings")
+//                 return
+//             }
+//
+//             call.resolve([
+//                 "value": implementation.updateWidgets(valueArray)
+//             ])
+        call.unimplemented("Not implemented on iOS.")
     }
 }
